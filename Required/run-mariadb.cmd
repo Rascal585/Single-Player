069120d9c19d834ce7b7f6a3969d4b9abbd3e438
Marwolf <cleako@gmail.com>
2019-06-19 23:46:03 -0400

    Major improvements

--- Required/run-mariadb.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-:# Open RSC: A replica RSC private server framework
-
-:# Path variables:
-SET mariadbpath="mariadb10.3.8\bin\"
-
-call START /min "" %mariadbpath%mysqld.exe --console
---
+++ Required/run-mariadb.ps1
@@ -0,0 +1,1 @@
+.\mariadb10.3.8\bin\mysqld.exe --console
